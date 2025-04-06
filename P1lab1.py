983a207ea86cc6f0acd6a886ee196830cbbc87d5
Alexj24004 <johnsona9735@student.faytechcc.edu>
2025-04-05 23:05:17 -0400

    Add files via upload

---
+++ P1lab1.py
@@ -0,0 +1,7 @@
+#Alexander johnson
+#4-5-2025
+#P1lab1
+#a script that is used to indroduce you to cti 110
+Firstname = input("enter your name: ")
+lastname = input("enter your lastname: ")
+ # print (f"hello, {firstname} {lastname}! Welcome to CTI-110  
